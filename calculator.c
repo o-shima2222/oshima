ea8052f212746cb7d2c845447208f28f3d0629c2
大島雄大 <ooshimayuudai@ooshimayuudainoiMac.local>
2025-02-09 14:47:24 +0900

    計算機ver1.0完成

--- calculator.c
+++ calculator.c
@@ -12,12 +12,49 @@
 int main(void)
 {
      int choice;
+     int kinds;
+     int x = 0;
+     int y = 0;;
+     int amount = 0;
+     int mod = 0;
 
-    printf("演算の種類を選んでください\n");
-     switch(choice){
-
+    printf("演算の種類を選んでください(1:加算 2:減算 3:乗算 4:除算)\n");
+    scanf("%d",&choice);
+     switch(choice)
+     {
         case 1:
-        printf("");
+        printf("加算を行います\n");
+        scanf("%d %d",&x,&y);
+        amount = x + y;
+        printf("%d",amount);
+        break;
 
+        case 2:
+        printf("減算を行います\n");
+        scanf("%d %d",&x,&y);
+        amount = x - y;
+        printf("%d",amount);
+        break;
+
+        case 3:
+        printf("乗算を行います\n");
+        scanf("%d %d",&x,&y);
+        amount = x * y;
+        printf("%d",amount);
+        break;
+
+        case 4:
+        printf("除算を行います\n");
+        scanf("%d %d",&x,&y);
+        amount = x / y;
+        mod = x % y;
+        printf("%d余り%d",amount,mod);
+        break;
+
+        default:
+        break;
         
      }
+
+     return 0;
+   }
