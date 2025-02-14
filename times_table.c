040e33a4559c972876e9e56c130550d31a511f27
大島雄大 <ooshimayuudai@ooshimayuudainoiMac.local>
2025-02-14 21:42:56 +0900

    九九の表をfor文で作成

---
+++ times_table.c
@@ -0,0 +1,23 @@
+/************************************************************
+ * Name ◆ Multiplication table                              *
+ * Details ◆  演算の九九　　　　　　　　 　　　　　　　　　　　　　　*
+ * Parameter ◆                                              *
+ * Return ◆                                                 *
+ *                                                          *
+ * [変更履歴]                                                *
+ * 2025/02/09 Y.Oshima 新規作成                              * 
+ ************************************************************/ 
+#include <stdio.h>
+
+int main(void)
+{
+    for(int i = 1; i <  10; i++)
+    {
+        for(int j = 1; j < 10; j++)
+        {
+            printf("    %2d",i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
