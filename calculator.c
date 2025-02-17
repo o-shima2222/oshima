0a924cf0abd2913eeb8f0bd722e95b078d2ee3a5
大島雄大 <ooshimayuudai@ooshimayuudainoiMac.local>
2025-02-17 20:46:51 +0900

    文字列制御によるループ制御の追加

--- calculator.c
+++ calculator.c
@@ -1,14 +1,16 @@
 /************************************************************
- * Name ◆Calculator                                            *
- * Details ◆  データを処理して計算や情報の管理を行う　　　　　　　　 *
- * Parameter ◆                                              *
- * Return ◆                                                 *
- *                                                          *
- * [変更履歴]                                                *
- * 2025/02/09 Y.Oshima 新規作成                              * 
- * 2025/02/16 Y.Oshima 変更
- ************************************************************/ 
+ * プログラム名: Calculator
+ * 説明       : データを処理して計算や情報の管理を行う
+ * パラメータ : なし
+ * 戻り値     : なし
+ * 
+ * [変更履歴]
+ * 2025/02/09 Y.Oshima 新規作成
+ * 2025/02/16 Y.Oshima 変更: 機能追加
+ * 2025/02/17 Y.Oshima 変更: 機能追加
+ ************************************************************/
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -17,31 +19,33 @@ int main(void)
      int y = 0;;
      int amount = 0;
      int mod = 0;
-
+     char trigger[10] = "";
+     do
+     { 
     printf("演算の種類を選んでください(1:加算 2:減算 3:乗算 4:除算)\n");
     scanf("%d",&choice);
 
     printf("2つの整数を入力してください\n");
     scanf("%d %d",&x,&y);
-
-     switch(choice)
+        
+      switch(choice)
      {
         case 1:
         printf("加算を行います\n");
         amount = x + y;
-        printf("%d",amount);
+        printf("%d\n",amount);
         break;
 
         case 2:
         printf("減算を行います\n");
         amount = x - y;
-        printf("%d",amount);
+        printf("%d\n",amount);
         break;
 
         case 3:
         printf("乗算を行います\n");
         amount = x * y;
-        printf("%d",amount);
+        printf("%d\n",amount);
         break;
 
         case 4:
@@ -54,7 +58,7 @@ int main(void)
         printf("除算を行います\n");
         amount = x / y;
         mod = x % y;
-        printf("%d余り%d",amount,mod);
+        printf("%d 余り %d\n",amount,mod);
         }
         break;
 
@@ -63,6 +67,10 @@ int main(void)
         break;
 
      }
-
+     printf("終了する場合は「qt」と入力してください\n");
+     scanf("%s",trigger);
+    }while(strcmp(trigger,"qt") != 0);
+    
      return 0;
-   }
+  }
+  
