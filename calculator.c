c6562ce2f84a03bba47778b76089958a61caac2b
大島雄大 <ooshimayuudai@ooshimayuudainoiMac.local>
2025-02-23 16:13:06 +0900

    演算の決定方法変更

--- calculator.c
+++ calculator.c
@@ -10,6 +10,7 @@
  * 2025/02/17 Y.Oshima 変更: 機能追加
  * 2025/02/18 Y.Oshima 変更: 機能追加
  * 2025/02/19 Y.Oshima 変更: 機能追加
+ * 2025/02/23 Y.Oshima 変更: コード修正
  ************************************************************/
 #include <stdio.h>
 #include <string.h>
@@ -19,7 +20,7 @@
 
 int main(void)
 {
-     int choice;
+     char operator;
      int x = 0;
      int y = 0;;
      int amount = 0;
@@ -30,33 +31,33 @@ int main(void)
 
      do
      { 
-    printf("演算の種類を選んでください(1:加算 2:減算 3:乗算 4:除算 5:累乗)\n");
-    scanf("%d",&choice);
+    printf("演算の種類を選んでください(+,-,*,/,^)\n");
+    scanf(" %c",&operator);
 
     printf("2つの整数を入力してください\n");
     scanf("%d %d",&x,&y);
         
-      switch(choice)
+      switch(operator)
      {
-        case 1:
+        case '+':
         printf("加算を行います\n");
         amount = x + y;
         printf("%d\n",amount);
         break;
 
-        case 2:
+        case '-':
         printf("減算を行います\n");
         amount = x - y;
         printf("%d\n",amount);
         break;
 
-        case 3:
+        case '*':
         printf("乗算を行います\n");
         amount = x * y;
         printf("%d\n",amount);
         break;
 
-        case 4:
+        case '/':
         if(y == 0)
         {
             printf("除算は0で割ることができません\n");
@@ -70,7 +71,7 @@ int main(void)
         }
         break;
 
-        case 5:
+        case '^':
         printf("累乗計算を行います\n");
         amount = pow(x,y);
         printf("%d\n",amount);
